f7af5d022fcc408857c511d9ceb76be35f35aaf1
Princy-Dumais <159206328+Princy-Dumais@users.noreply.github.com>
2024-03-11 14:13:41 +1100

    Update Script JS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-let Myalert = 12;
-let enterName = 100;
-if (Myalert < enterName) {
-alert(Myalert * enterName);
+let enterName = prompt("Enter your name");
+
+let playerFeedback = "test";
+
+loadDIV();
+
+
+
+function loadDIV () {
+    document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>";
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 and 10</H2>";
+    document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback;
 }
