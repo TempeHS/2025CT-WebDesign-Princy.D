421e025d6de82367a7016d84ce85c5644628c965
Princy-Dumais <159206328+Princy-Dumais@users.noreply.github.com>
2024-03-13 15:03:52 +1100

    adding RNG (random number generator)

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let enterName = prompt("Enter your name");
+let playerGuess; 
+let computerGuess = randomNum();
+let playerFeedback = computerGuess;
+loadDIV()
 
-let playerFeedback = "test";
+function randomNum() {
+    return Math.floor(Math.random() * (10 - 1 + 1 ) + 1)
+}
 
 loadDIV();
-
 
 
 function loadDIV () {
     document.getElementById("ScriptThisDIV").innerHTML = "<H1>Hello " + enterName + "</H1>";
     document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + "<H2>Guess a number between 1 and 10</H2>";
     document.getElementById("ScriptThisDIV").innerHTML = document.getElementById("ScriptThisDIV").innerHTML + playerFeedback;
+}
+
+function enternumber () {
+    playerGuess = prompt ("Enter a number")
 }
